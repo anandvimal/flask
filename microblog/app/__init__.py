44e5a5f9eef9e9a3b950d69f3e6f7cf8fc08d5af
deepak <colirsweb@gmail.com>
2019-08-04 21:04:12 -0700

    ch3 basic forms working
    no data base with app but its working at least.

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+#app.config['SECRET KEY'] = 'you-will-never-guess'
+#we can add more variables here as needed
 
 from app import routes
