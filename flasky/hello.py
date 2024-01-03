39dda189145e892575fc87b8e6b0ba30fe8c2ed4
Deepak Sandhu <deepaksandhu789@gmail.com>
2024-01-03 13:25:17 -0800

    added request to see user agent in / route

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Hello World! your User-Agent is {}</h1>'.format(user_agent)
 
 @app.route('/user/<name>')
 def user(name):
