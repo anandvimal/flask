57839385309a2fa65da0c1c8babda90d8836e7fe
Deepak Sandhu <deepaksandhu789@gmail.com>
2024-01-29 17:41:00 -0800

    init db in sqlite

--- sqlalchemy/retrofun/db.py
+++ sqlalchemy/retrofun/db.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv()
 
+engine = create_engine(os.environ['DATABASE_URL'], echo=True)
 print('Database URL:', os.environ['DATABASE_URL'])
